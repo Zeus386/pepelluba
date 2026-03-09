4216c49a606b84e727c7685d3fad3dced86877cb
zeus386 <73391076+Zeus386@users.noreply.github.com>
2026-03-09 12:08:57 +0100

    [Admin Panel] Update main_config.js

--- js/data/main_config.js
+++ js/data/main_config.js
@@ -13,38 +13,40 @@ const EXERCISES_DATA = {
             "exe7": { title: "M ⟶ H, M ⟶ T, M ⊢ H ∧ T", name: "1_7_auto", defaultMethod: "Solución Automática (auto)" },
             "exe8": { title: "A ⟶ T, T ⟶ F, C ⊢ A ⟶ F", name: "1_8_auto", defaultMethod: "Solución Automática (auto)" },
             "exe9": { title: "A ⟶ R, R ⟶ I ⊢ A ⟶ I", name: "1_9_auto", defaultMethod: "Solución Automática (auto)" },
-            "exe10":{ title: "S ⟶ E, S ⟶ D ⊢ S ⟶ E ∧ D", name: "1_10_auto", defaultMethod: "Solución Automática (auto)" },
-            "exe11":{ title: "E ∨ S, E ⟶ R, S ⟶ R ⊢ R", name: "1_11_auto", defaultMethod: "Solución Automática (auto)" },
-            "exe12":{ title: "C ⟶ L, L ⟶ S, S ⟶ (P ∧ L) ⊢ C ⟶ L", name: "1_12_auto", defaultMethod: "Solución Automática (auto)" },
-            "exe13":{ title: "A ⟶ F, M ⟶ A, P ⟶ M ⊢ P ⟶ F", name: "1_13_auto", defaultMethod: "Solución Automática (auto)" },
-            "exe14":{ title: "J ⟶ M, C ⟶ A, J ∨ C ⊢ M ∨ A", name: "1_14_auto", defaultMethod: "Solución Automática (auto)" }
+            "exe10": { title: "S ⟶ E, S ⟶ D ⊢ S ⟶ E ∧ D", name: "1_10_auto", defaultMethod: "Solución Automática (auto)" },
+            "exe11": { title: "E ∨ S, E ⟶ R, S ⟶ R ⊢ R", name: "1_11_auto", defaultMethod: "Solución Automática (auto)" },
+            "exe12": { title: "C ⟶ L, L ⟶ S, S ⟶ (P ∧ L) ⊢ C ⟶ L", name: "1_12_auto", defaultMethod: "Solución Automática (auto)" },
+            "exe13": { title: "A ⟶ F, M ⟶ A, P ⟶ M ⊢ P ⟶ F", name: "1_13_auto", defaultMethod: "Solución Automática (auto)" },
+            "exe14": { title: "J ⟶ M, C ⟶ A, J ∨ C ⊢ M ∨ A", name: "1_14_auto", defaultMethod: "Solución Automática (auto)" },
         }
     },
     "2": {
         title: "Tema 2: Semántica y Tablas de Verdad",
         exercises: {
-            "exe1": { title: "Ejercicio de Ejemplo", name: "2_01_ejemplo", defaultMethod: "Deducción Natural" }
         }
     },
     "3": {
-         title: "Tema 3: Deducción Natural",
-         exercises: {
-             "exe15": { title: "Ejercicio 15 (Deducción)", name: "3_15", defaultMethod: "Deducción por Tareas" },
-             "exe17": { title: "Ejercicio 17 (Deducción)", name: "3_17", defaultMethod: "Deducción por Tareas" }
-         }
+        title: "Tema 3: Deducción Natural",
+        exercises: {
+            "exe15": { title: "Ejercicio 15 (Deducción)", name: "3_15", defaultMethod: "Deducción por Tareas" },
+            "exe17": { title: "Ejercicio 17 (Deducción)", name: "3_17", defaultMethod: "Deducción por Tareas" },
+        }
     },
     "4": {
         title: "Tema 4: Lógica de Predicados Básica",
-        exercises: {}
+        exercises: {
+        }
     },
     "5": {
         title: "Tema 5: Semántica de Predicados",
-        exercises: {}
+        exercises: {
+        }
     },
     "6": {
         title: "Tema 6: Deducción Natural Numérica",
-        exercises: {}
-    }
+        exercises: {
+        }
+    },
 };
 
 window.EXERCISES_DATA = EXERCISES_DATA;

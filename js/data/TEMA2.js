7e10dd8d5fde14dedde6d48be955716b0f2b70b0
zeus386 <73391076+Zeus386@users.noreply.github.com>
2026-03-09 12:17:24 +0100

    [Admin Panel] Update TEMA2.js

--- js/data/TEMA2.js
+++ js/data/TEMA2.js
@@ -1,6 +1,17 @@
 // TEMA2.js - Pruebas para Tema 2
 
 const TEMA2_PROOFS = {
+    "2_1": {
+        title: "Tema 2 - AAA",
+        steps: [
+            {
+                code: "A",
+                explanation: "A",
+                activeHyp: [],
+                highlights: ["AAA"]
+            },
+        ]
+    },
 };
 
 window.TEMA2_PROOFS = TEMA2_PROOFS;
